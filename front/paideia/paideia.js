10b3948932be44cbbe9f54cc66561e017359ef6e
jiyoon-na <164995957+jiyoon-na@users.noreply.github.com>
2024-06-23 13:36:45 +0900

    feat :경로수정

--- front/paideia/paideia.js
+++ front/paideia/paideia.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = {
-    homeButton: "/front/paideia/paideia.html",
-    graphButton: "/front/graph/graph.html",
-    progressButton: "/front/progress/progress.html",
-    settingButton: "/front/set/setting.html",
+    homeButton: "/paidieia/front/paideia/paideia.html",
+    graphButton: "/paidieia/front/graph/graph.html",
+    progressButton: "/paidieia/front/progress/progress.html",
+    settingButton: "/paidieia/front/set/setting.html",
   };
 
   let currentUserType = localStorage.getItem("userType") || "default";
